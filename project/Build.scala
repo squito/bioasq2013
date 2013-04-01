ec103d7f69d07e0cdeeb26b548bbc1c8037f375a
Imran Rashid <imran@quantifind.com>
2013-04-01 09:04:13 -0700

    sbt setup

---
+++ project/Build.scala
@@ -0,0 +1,35 @@
+import sbt._
+import Keys._
+
+object SparkBuild extends Build {
+  lazy val core = Project("core", file("."), settings = coreSettings)
+
+  def sharedSettings = Defaults.defaultSettings ++ Seq(
+    version := "0.1-SNAPSHOT",
+    scalaVersion := "2.9.1",
+    organization := "com.imranrashid",
+    scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
+    unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
+    retrieveManaged := true,
+    transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
+    libraryDependencies ++= Seq(
+      "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+    )
+  )
+
+  val slf4jVersion = "1.6.1"
+
+  def coreSettings = sharedSettings ++ Seq(
+    name := "bioasq2013",
+    resolvers ++= Seq(
+      "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "JBoss Repository" at "http://repository.jboss.org/nexus/content/repositories/releases/"
+    ),
+    libraryDependencies ++= Seq(
+      "log4j" % "log4j" % "1.2.16",
+      "org.slf4j" % "slf4j-api" % slf4jVersion,
+      "org.slf4j" % "slf4j-log4j12" % slf4jVersion
+    )
+  )
+
+}
