d5edba1f98eb0d043ebc4c0351f86ce91022ec9b
Imran Rashid <imran@quantifind.com>
2013-04-03 23:28:17 -0700

    setup basic featurization

--- src/main/scala/bioasq/parsers/AbstractParser.scala
+++ src/main/scala/bioasq/parsers/AbstractParser.scala
@@ -1,43 +1,59 @@
 package bioasq.parsers
 
 import io.Source
-import com.fasterxml.jackson.core.{ObjectCodec, Base64Variant, JsonParser}
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.util.zip.GZIPInputStream
 import java.io.FileInputStream
 import bioasq.DataFiles
 import collection._
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import java.util.Date
+import org.sblaj.io.VectorFileSet
+import org.sblaj.featurization.{Murmur64, MurmurFeaturizer, FeaturizerHelper, BinaryFeaturizer}
 
 /**
  *
  */
 
-class AbstractParser {
-
-}
-
 object AbstractParser {
 
   def main(args: Array[String]) {
     val in = new GZIPInputStream(new FileInputStream(DataFiles.TrainingAbstractsGzip))
-    parseAbstracts(Source.fromInputStream(in)){abs => println(abs + "\t" + abs.meshMajor.mkString(","))}
+    new java.io.File(DataFiles.TrainingFeaturizedDir).mkdirs()
+    featurizeAbstracts(Source.fromInputStream(in), VectorFileSet.forDir(DataFiles.TrainingFeaturizedDir))
+  }
+
+  def featurizeAbstracts(abstractSource: Source, featurizationFiles: VectorFileSet) {
+    val abstractsItr = new ItrWithLogs[Abstract](parseAbstracts(abstractSource),1e6.toInt,{
+      (abs, idx) => if (idx % 1000 == 0) println(new Date() + "\t" + idx)
+    })
+    FeaturizerHelper.featurizeToFiles(abstractsItr, AbstractFeaturizer, featurizationFiles)
   }
 
   def parseAbstracts(file:String)(f: Abstract => Unit) {
-    parseAbstracts(Source.fromFile(file))(f)
+    parseAbstracts(Source.fromFile(file)).foreach(f)
   }
 
-  def parseAbstracts(source: Source)(f: Abstract => Unit) {
+  def parseAbstracts(source: Source): Iterator[Abstract] = {
     val om = new ObjectMapper()
     om.registerModule(DefaultScalaModule)
     source.getLines().zipWithIndex.
       filter{case (line, idx) => idx != 0 && !line.trim.equals("}")}.
-      foreach{ case (line, _) =>
-        val abs = om.readValue(line, classOf[Abstract])
-        f(abs)
+      map{ case (line, _) =>
+        om.readValue(line, classOf[Abstract])
       }
   }
+}
+
+class ItrWithLogs[T](val base: Iterator[T], val max: Int = Int.MaxValue, logF: (T,Int) => Unit) extends Iterator[T] {
+  var idx = 0
+  def next = {
+    val n = base.next()
+    logF(n, idx)
+    idx += 1
+    n
+  }
+  def hasNext = {idx < max && base.hasNext}
 }
 
 
@@ -49,3 +65,11 @@ case class Abstract(
   val title: String,
   val year: String  //for some reason, its a year in the json
 )
+
+object AbstractFeaturizer extends MurmurFeaturizer[Abstract] {
+  override def getId(abs: Abstract) = Murmur64.hash64(abs.pmid)
+  override def extractor(abs: Abstract) =
+    abs.abstractText.split("\\s").toSet ++ //unigrams
+      Set("JOURNAL:" + abs.journal) ++  //journal
+      abs.meshMajor.map{"MESH:" + _}.toSet //mesh terms
+}
